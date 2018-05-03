9edcb43f9b1da1d4954d08bb788f3d0e477e33bc
mproscia <marisa.proscia1@marist.edu>
2018-05-03 16:21:08 -0400

    making sure to have everything in both typescript and javascript

---
+++ source/scripts/readFile.js
@@ -0,0 +1,9 @@
+function loadFileAsText() {
+    var fileToLoad = document.getElementById("fileToLoad").files[0];
+    var fileReader = new FileReader();
+    fileReader.onload = function (fileLoadedEvent) {
+        var textFromFileLoaded = fileLoadedEvent.target.result;
+        document.getElementById("content-target").value = textFromFileLoaded;
+    };
+    fileReader.readAsText(fileToLoad, "UTF-8");
+}
