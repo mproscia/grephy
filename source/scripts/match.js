b7491e449ef70694c2487c5a1291cfebfa811498
mproscia <marisa.proscia1@marist.edu>
2018-05-03 16:19:28 -0400

    file was missing

---
+++ source/scripts/match.js
@@ -0,0 +1,12 @@
+var grephy;
+(function (grephy) {
+    var Match = /** @class */ (function () {
+        function Match() {
+        }
+        Match.init = function () {
+            // reset matching variables
+        };
+        return Match;
+    }());
+    grephy.Match = Match;
+})(grephy || (grephy = {}));
