ff6b45f8641109fe1ebc170ce08fceda7003c043
mproscia <marisa.proscia1@marist.edu>
2018-05-06 17:18:29 -0400

    adding functionality to error if input is not found in regex alphabet

--- source/scripts/consume.js
+++ source/scripts/consume.js
@@ -9,22 +9,42 @@ var grephy;
         Consume.consumeInput = function () {
             this.verifyInput();
             if (matched == true) {
+                document.getElementById("matchButton").disabled = true;
                 grephy.Match.matchTokens();
             }
             else {
-                document.getElementById("content-target").value = "Input does NOT Match Regex";
+                if (acceptedChar.length == 0) {
+                    // do nothing bc output already printed
+                    document.getElementById("content-target").value = "Regex is Empty - Try Again";
+                }
+                else {
+                    document.getElementById("content-target").value = "Input does NOT Match Regex - Try Again";
+                    document.getElementById("matchButton").disabled = true;
+                    document.getElementById("readButton").disabled = false;
+                }
             }
         };
         Consume.verifyInput = function () {
             this.getRegex();
+            // check that input can match regex
+            for (var k = 0; k < inputLength; k++) {
+                for (var l = 0; l < acceptedChar.length; l++) {
+                    if (input[k] == acceptedChar[l]) {
+                        matched = true;
+                        console.log("match");
+                    }
+                }
+            }
         };
         Consume.getRegex = function () {
             var regex = document.getElementById("regexTA").value;
-            // get rid of special characters for terms of matching
-            for (var i = 0; i < regex.length; i++) {
-                for (var j = 0; j < alphabet.length; j++) {
-                    if (regex.charAt(i) == alphabet[j]) {
-                        acceptedChar.push(regex.charAt(i));
+            if (regex != "") {
+                // get rid of special characters for terms of matching
+                for (var i = 0; i < regex.length; i++) {
+                    for (var j = 0; j < alphabet.length; j++) {
+                        if (regex.charAt(i) == alphabet[j]) {
+                            acceptedChar.push(regex.charAt(i));
+                        }
                     }
                 }
             }
--- source/scripts/consume.ts
+++ source/scripts/consume.ts
@@ -9,29 +9,49 @@ module grephy {
         public static consumeInput(){
             this.verifyInput();
             if(matched == true){
+                (<HTMLInputElement>document.getElementById("matchButton")).disabled = true;
                 grephy.Match.matchTokens();
             } else {
-                (<HTMLInputElement>document.getElementById("content-target")).value = "Input does NOT Match Regex";
-
+                if (acceptedChar.length == 0){
+                    // do nothing bc output already printed
+                    (<HTMLInputElement>document.getElementById("content-target")).value = "Regex is Empty - Try Again";
+                } else {
+                    (<HTMLInputElement>document.getElementById("content-target")).value = "Input does NOT Match Regex - Try Again";
+                    (<HTMLInputElement>document.getElementById("matchButton")).disabled = true;
+                    (<HTMLInputElement>document.getElementById("readButton")).disabled = false;
+                }
             }
         }
 
         public static verifyInput(){
            this.getRegex();
 
+            // check that input can match regex
+
+            for(var k = 0; k < inputLength; k++){
+                for (var l = 0; l < acceptedChar.length; l++){
+                    if(input[k] == acceptedChar[l]){
+                        matched = true;
+                        console.log("match");
+                    }
+                }
+            }
         }
 
         public static getRegex(){
             var regex = (<HTMLInputElement>document.getElementById("regexTA")).value;
-
-            // get rid of special characters for terms of matching
-            for (var i = 0; i < regex.length; i ++){
-                for (var j = 0; j < alphabet.length; j++){
-                    if (regex.charAt(i) == alphabet[j]){
-                        acceptedChar.push(regex.charAt(i));
+            if(regex != ""){
+                // get rid of special characters for terms of matching
+                for (var i = 0; i < regex.length; i ++){
+                    for (var j = 0; j < alphabet.length; j++){
+                        if (regex.charAt(i) == alphabet[j]){
+                            acceptedChar.push(regex.charAt(i));
+                        }
                     }
                 }
             }
+
+
 
             console.log("accept" + acceptedChar);
 
@@ -57,14 +77,14 @@ module grephy {
                 (<HTMLInputElement>document.getElementById("readButton")).disabled = true;
                 (<HTMLInputElement>document.getElementById("matchButton")).disabled = false;
 
+
             }
-
-
-
 
 
             //TODO: capability to read more than one line
         }
+
+
 
         public static reload(){
             window.location.reload();
